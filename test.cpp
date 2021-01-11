e24185018bbc6dac8751b2855a302bc2d3e4917e
danams101 <danams101@yahoo.com>
2021-01-10 17:39:37 -0800

    Added Three More Tests
    *Added OneTwoThree test
    *Added LongPhrase test
    *Added Greetings test

--- test.cpp
+++ test.cpp
@@ -11,6 +11,45 @@ TEST(EchoTest, HelloWorld) {
 	EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+	char* test_val[1];
+	test_val[0] = "./c-echo";
+
+	EXPECT_EQ("", echo(1, test_val));
+}
+
+TEST(EchoTest, OneTwoThree) {
+	char* test_val[5];
+	test_val[0] = "./c-echo";
+        test_val[1] = "1";
+        test_val[2] = "2";
+	test_val[3] = "3";
+	test_val[4] = "!";
+
+	EXPECT_EQ("1 2 3 !", echo(5, test_val));
+}
+
+TEST(EchoTest, LongPhrase) {
+	char* test_val[2];
+	test_val[0] = "./c-echo";
+	test_val[1] = "ThisIsAReallyLongPhraseWithoutSpaces.";
+
+	EXPECT_EQ("ThisIsAReallyLongPhraseWithoutSpaces.", echo(2, test_val));
+}
+
+TEST(EchoTest, Greeting) {
+	char* test_val[7];
+	test_val[0] = "./c-echo";
+        test_val[1] = "Hi,";
+        test_val[2] = "How";
+        test_val[3] = "are";
+	test_val[4] = "you";
+        test_val[5] = "doing";
+        test_val[6] = "today?";
+        
+	EXPECT_EQ("Hi, How are you doing today?", echo(7,test_val));
+}
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
